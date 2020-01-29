03cd33e4c9f4262acfc71dfe93aaa488bd53febe
sinovdeath <sin-ov-death@yandex.ru>
2020-01-29 10:37:22 +0200

    #Task1

--- Tasks/Task1/Task1_1/CalculateArea.py
+++ Tasks/Task1/Task1_1/CalculateArea.py
@@ -1,8 +1,13 @@
-from Tasks.Task1.Task1_1.Сircle import Сircle
+from Tasks.Task1.Task1_1.Сircle import Сircle #Импорт класса
 
-firstCircle = Сircle()
-radius = int(input("Set radius: "))
-firstCircle.setRadius(radius)
+firstCircle = Сircle() #Создание экземпляра класса. Присвоение класса переменной, чтобы мы могли записать в память и манипулировать классом в дальнейшем.
 
-firstCircle.calculateArea(radius)
-firstCircle.printResult()
+print("Радиус до того как мы его засетали: " + str(firstCircle.getRadius()))
+
+radius = int(input("Введите радиус: ")) #Вызываем сканер экрана, чтобы считать значение, введённое пользователем и записываем его в переменную radius.
+firstCircle.setRadius(radius) #Устанавливаем значение через сеттер, чтобы сделать расчёт в дальнейшем.
+
+print("А это уже после того как мы указали радиус: " + str(firstCircle.getRadius()))
+
+firstCircle.calculateArea(firstCircle.getRadius()) #Теперь введённое значение вставляем в функцию с помощью геттера.
+firstCircle.printResult() #Выводим ответ на экран.
